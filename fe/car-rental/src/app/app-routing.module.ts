1a1a657dd2c277cf9fa9b33773e2facb43d55da3
LeQuocNhatNam <lequocnhatnam07041994@gmail.com>
2023-04-18 13:26:49 +0700

    s

--- fe/car-rental/src/app/app-routing.module.ts
+++ fe/car-rental/src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {LoginComponent} from "./login/login.component";
+import {CarDetailComponent} from "./car-detail/car-detail.component";
+import {CartComponent} from "./cart/cart.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomePageComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'detail',
+  component: CarDetailComponent
+}, {
+  path: 'cart',
+  component: CartComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- fe/car-rental/src/app/app.module.ts
+++ fe/car-rental/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CartComponent } from './cart/cart.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { HomePageComponent } from './home-page/home-page.component';
     LoginComponent,
     HeaderComponent,
     FooterComponent,
-    HomePageComponent
+    HomePageComponent,
+    CartComponent,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule,
