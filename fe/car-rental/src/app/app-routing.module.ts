8aa1c5622bf4116d3758823427f57098eb8f7cbe
LeQuocNhatNam <lequocnhatnam07041994@gmail.com>
2023-04-24 09:47:49 +0700

    NamLQN updates

--- fe/car-rental/src/app/app-routing.module.ts
+++ fe/car-rental/src/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomePageComponent} from "./home-page/home-page.component";
-import {LoginComponent} from "./login/login.component";
 import {CarDetailComponent} from "./car-detail/car-detail.component";
 import {CartComponent} from "./cart/cart.component";
 
 
-const routes: Routes = [{
-  path: '',
-  component: HomePageComponent
-}, {
-  path: 'login',
-  component: LoginComponent
-}, {
-  path: 'detail',
-  component: CarDetailComponent
-}, {
-  path: 'cart',
-  component: CartComponent
-}];
+const routes: Routes = [
+  {
+    path: 'security',
+    loadChildren: () => import('./security-authentication/security-authentication.module')
+      .then(module => module.SecurityAuthenticationModule),
+  },
+  {
+    path: '',
+    component: HomePageComponent
+  },
+  {
+    path: 'detail',
+    component: CarDetailComponent
+  }, {
+    path: 'cart',
+    component: CartComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fe/car-rental/src/app/app.module.ts
+++ fe/car-rental/src/app/app.module.ts
@@ -2,17 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CartComponent } from './cart/cart.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HeaderComponent,
     FooterComponent,
     HomePageComponent,
@@ -21,7 +20,8 @@ import { CarDetailComponent } from './car-detail/car-detail.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
